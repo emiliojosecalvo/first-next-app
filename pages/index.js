46ca83f5cf2602ed0147d4e99358e6b6d2f5b794
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-21 20:55:13 +0000

    fetching post with getInitialsProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
+import axios from 'axios';
 
-const Index = () => {
+const Index = ({ post }) => {
     return (
         <div  >
             <h1>Hello Emilio</h1>
+            <ul>
+                {post.map(p => {
+                    return <li key={p.id}>{p.title}</li>
+                })}
+            </ul>
         </div >
     )
+}
+
+Index.getInitialProps = async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const { data } = response;
+    return { post: data }
 }
 
 export default Index;
