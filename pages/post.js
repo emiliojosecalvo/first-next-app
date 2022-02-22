0ea85a8799fec5c6ca1a6d6fbb51757ad6c99ca3
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-22 23:27:20 +0000

    routing using withRouter

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,14 @@
-
-const Post = ({ id }) => {//get the id from the query string as a prop
+import { withRouter } from 'next/router';
+const Post = (props) => {//get the id from the query string as a prop
     return (
         <>
-            <h1>ID of the post #{id}</h1>
+            <h1>ID of the post #{props.router.query.id}</h1>
         </>
     )
 };
 
-Post.getInitialProps = async ({ query }) => {//get data from the query
-    return query;//return all the data from the query as a prop
-}
+// Post.getInitialProps = async ({ query }) => {//get data from the query
+//     return query;//return all the data from the query as a prop
+// }
 
-export default Post;
+export default withRouter(Post);
