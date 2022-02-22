f9ebb6fa8aea485157ae0d813e5dacac7cc69da0
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-22 23:38:06 +0000

    using initialprops to route

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,14 @@
 import { withRouter } from 'next/router';
-const Post = (props) => {//get the id from the query string as a prop
+const Post = ({ id }) => {
     return (
         <>
-            <h1>ID of the post #{props.router.query.id}</h1>
+            <h1>ID of the post #{id}</h1>
         </>
     )
 };
 
-// Post.getInitialProps = async ({ query }) => {//get data from the query
-//     return query;//return all the data from the query as a prop
-// }
+Post.getInitialProps = async ({ query }) => {
+    return query;
+}
 
-export default withRouter(Post);
+export default Post;
