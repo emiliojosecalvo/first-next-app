f6a026f59f6c076a9c9d17a8b18a0edbdde0b7a1
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-23 13:48:42 +0000

    adding server file

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Index = ({ posts }) => {
             <h1>Hello Emilio</h1>
             <ul>
                 {posts.map(post => {
-                    return <li key={post.id}><Link href={`/post?id=${post.id}`}><a>{post.title}</a></Link></li>
+                    return <li key={post.id}><Link href={`/post?id=${post.id}`} as={`/p/${post.id}`}><a>{post.title}</a></Link></li>
                 })}
             </ul>
         </div >
---
+++ server1.js
@@ -0,0 +1,9 @@
+const express = requier('express');
+const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+
+})
