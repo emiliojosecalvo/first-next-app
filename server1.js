1f95b722a080abac5c1d303efa0509a973df7d7d
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-24 11:02:07 +0000

    adding routers

--- server1.js
+++ server1.js
@@ -1,9 +1,22 @@
-const express = requier('express');
+const express = require('express');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
+    const server = express();
 
-})
+    server.get('/p/:id', (req, res) => {
+        app.render(req, res, '/post', { id: 11 });
+    });
+
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    server.listen(3000, err => {
+        if (err) throw err;
+        console.log('NOW SERVING ON Localhost:3000');
+    });
+});
